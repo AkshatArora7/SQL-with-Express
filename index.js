37b26af2b58bd81b0f9b3f7709952a83ebf1ec04
Akshat Arora <akshat.arora456@gmail.com>
2023-05-04 11:04:15 -0400

    added questions

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const express = require("express");
 
 const app = express();
 
-
-
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -39,7 +36,7 @@ const menuOptions = [
     "Exit",
   ];
 
-  const menuQuestions = [
+  const menuPrompts = [
     {
       type: "list",
       name: "choice",
@@ -48,17 +45,17 @@ const menuOptions = [
     },
   ];
 
-  const addRoleQuestions = [
+  const addRolePrompts = [
     { type: "input", name: "name", message: "What is role name?" },
     { type: "input", name: "salary", message: "What is role salary?" },
     {
       type: "list",
       name: "department_id",
       message: "Which department?",
-      choices: allDepartments,
+      choices: [],
     },
   ];
-  const addEmployeeQuestions = [
+  const addEmployeePrompts = [
     {
       type: "input",
       name: "first_name",
@@ -79,7 +76,49 @@ const menuOptions = [
       type: "list",
       name: "manager_id",
       message: "Who's employee's manager?",
-      choices: allManagers2,
+      choices: [],
+    },
+  ];
+
+  const deleteEmployeePrompts = [
+    {
+      type: "list",
+      name: "chosenOption",
+      message: "Which employee would you like to delete?",
+      choices: allManagers,
+    },
+  ];
+  const deleteRolePrompts = [
+    {
+      type: "list",
+      name: "chosenOption",
+      message: "Which role would you like to delete?",
+      choices: allRoles,
+    },
+  ];
+  const deleteDepartmentPrompts = [
+    {
+      type: "list",
+      name: "chosenOption",
+      message: "Which department would you like to delete?",
+      choices: allDepartments,
+    },
+  ];
+
+  const viewManagerPrompts = [
+    {
+      type: "list",
+      name: "managerToBeViewed",
+      message: "Which manager would you like to view?",
+      choices: allManagers,
+    },
+  ];
+  const viewDepartmentPrompts = [
+    {
+      type: "list",
+      name: "departmentToBeViewed",
+      message: "Which department would you like to view?",
+      choices: allDepartments,
     },
   ];
 
