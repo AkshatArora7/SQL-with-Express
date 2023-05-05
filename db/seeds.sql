b93ac1601acc459343c0b1b6447b2edf1fcf8b06
Akshat Arora <akshat.arora456@gmail.com>
2023-05-04 21:14:27 -0400

    bugs fixed

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,7 +11,7 @@ VALUES ("Graphic Designer",40000.00,3),
        ("Financial Analyst",60000.00,2),
        ("Sales Executive",80000.00,1),
        ("Web Developer",140000.00,1),
-       ("Content Writer",110000.00,4)
+       ("Content Writer",110000.00,4),
        ("Business Analyst",12000.00,3),
        ("Quality Assurance Analyst",200000.00,4),
        ("Legal Counsel",560000.00,1);
@@ -19,11 +19,11 @@ VALUES ("Graphic Designer",40000.00,3),
 
 
 INSERT INTO employee ( first_name, last_name, role_id, manager_id)
-VALUES ("Samuel","Johnson",1,1),
-       ( "Harper","Williams",2,2),
-       ( "Abigail","Brown",3,3),
-       ( "Ava","Smith",4,7),
-       ( "Mia","Garcia",5,1),
-       ( "Daniel","Ramirez",6,NULL),
-       ( "Jacob","Gonzalez",7,2),
-       ( "Abigail","Carter",8,NULL);
+VALUES ("Samuel","Johnson",1,NULL),
+       ( "Harper","Williams",2,1),
+       ( "Abigail","Brown",3,2),
+       ( "Ava","Smith",4,3),
+       ( "Mia","Garcia",5,4),
+       ( "Daniel","Ramirez",6,5),
+       ( "Jacob","Gonzalez",7,6),
+       ( "Abigail","Carter",7,7);
--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
-const express = require("express");
-
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-
 
 const db = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "",
   database: "employeeTrackerDB",
@@ -55,6 +47,8 @@ const menuOptions = [
       choices: [],
     },
   ];
+  
+  let roles=[];
   const addEmployeePrompts = [
     {
       type: "input",
@@ -70,7 +64,7 @@ const menuOptions = [
       type: "list",
       name: "role_id",
       message: "What is employee's role?",
-      choices: allRoles,
+      choices: roles,
     },
     {
       type: "list",
@@ -80,12 +74,13 @@ const menuOptions = [
     },
   ];
 
+  let managers=[];
   const deleteEmployeePrompts = [
     {
       type: "list",
       name: "chosenOption",
       message: "Which employee would you like to delete?",
-      choices: allManagers,
+      choices: managers,
     },
   ];
   const deleteRolePrompts = [
@@ -93,24 +88,25 @@ const menuOptions = [
       type: "list",
       name: "chosenOption",
       message: "Which role would you like to delete?",
-      choices: allRoles,
+      choices: roles,
     },
   ];
+  let departments=[];
   const deleteDepartmentPrompts = [
     {
       type: "list",
       name: "chosenOption",
       message: "Which department would you like to delete?",
-      choices: allDepartments,
+      choices: departments,
     },
   ];
-
+  
   const viewManagerPrompts = [
     {
       type: "list",
       name: "managerToBeViewed",
       message: "Which manager would you like to view?",
-      choices: allManagers,
+      choices: managers,
     },
   ];
   const viewDepartmentPrompts = [
@@ -118,7 +114,45 @@ const menuOptions = [
       type: "list",
       name: "departmentToBeViewed",
       message: "Which department would you like to view?",
-      choices: allDepartments,
+      choices: departments,
     },
   ];
 
+
+  async function init(){
+    const chosenOption = await inquirer.prompt(menuPrompts);
+    switch (chosenOption.choice){
+      case menuOptions[0]:
+        console.log("View All Employees")
+      case menuOptions[1]:
+        console.log(menuOptions[1])
+      case menuOptions[2]:
+        console.log(menuOptions[2])
+      case menuOptions[3]:
+        console.log(menuOptions[3])
+      case menuOptions[4]:
+        console.log(menuOptions[4])
+      case menuOptions[5]:
+        console.log(menuOptions[5])
+      case menuOptions[6]:
+        console.log(menuOptions[6])
+      case menuOptions[7]:
+        console.log(menuOptions[7])
+      case menuOptions[8]:
+        console.log(menuOptions[8])
+      case menuOptions[9]:
+        console.log(menuOptions[9])
+      case menuOptions[10]:
+        console.log(menuOptions[10])
+      case menuOptions[11]:
+        console.log(menuOptions[11])
+      case menuOptions[12]:
+        console.log(menuOptions[12])
+      case menuOptions[13]:
+        console.log(menuOptions[13])
+      case menuOptions[14]:
+        process.exit();
+    }
+}
+
+init();
