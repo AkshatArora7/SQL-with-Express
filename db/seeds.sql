9a98d261de807a8765068210485d8258fa8cc819
Akshat Arora <akshat.arora456@gmail.com>
2023-05-06 11:05:37 -0400

    created add employee, role, department

--- db/seeds.sql
+++ db/seeds.sql
@@ -20,10 +20,10 @@ VALUES ("Graphic Designer",40000.00,3),
 
 INSERT INTO employee ( first_name, last_name, role_id, manager_id)
 VALUES ("Samuel","Johnson",1,NULL),
-       ( "Harper","Williams",2,1),
-       ( "Abigail","Brown",3,2),
-       ( "Ava","Smith",4,3),
-       ( "Mia","Garcia",5,4),
-       ( "Daniel","Ramirez",6,5),
-       ( "Jacob","Gonzalez",7,6),
-       ( "Abigail","Carter",7,7);
+       ( "Harper","Williams",2,NULL),
+       ( "Abigail","Brown",3,NULL),
+       ( "Ava","Smith",4,1),
+       ( "Mia","Garcia",5,NULL),
+       ( "Daniel","Ramirez",6,NULL),
+       ( "Jacob","Gonzalez",7,2),
+       ( "Abigail","Carter",7,2);
